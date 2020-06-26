c140eadd00601836a8de14401fbce9285bb74034
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-06-26 12:54:01 +0100

    st works with swallow patch

--- config.h
+++ config.h
@@ -31,6 +31,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         0,          0,           0,        -1 }, // floating got annoying
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,           1,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
