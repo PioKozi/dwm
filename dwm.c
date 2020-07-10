8f0b6740bdcef7957be4b79d637d1063f19f05c7
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-07-10 20:52:52 +0100

    added attachaside patch

--- dwm.c
+++ dwm.c
@@ -51,7 +51,8 @@
 #define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
 #define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
                                * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
-#define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]) || C->issticky)
+#define ISVISIBLEONTAG(C, T)    ((C->tags & T))
+#define ISVISIBLE(C)            ISVISIBLEONTAG(C, C->mon->tagset[C->mon->seltags] || C->issticky)
 #define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
@@ -157,6 +158,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interac
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void attachaside(Client *c);
 static void attachstack(Client *c);
 static void buttonpress(XEvent *e);
 static void bstack(Monitor *m);
@@ -198,6 +200,7 @@ static void monocle(Monitor *m);
 static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
 static void movestack(const Arg *arg);
+static Client *nexttagged(Client *c);
 static Client *nexttiled(Client *c);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
@@ -444,6 +447,18 @@ attach(Client *c)
 {
 	c->next = c->mon->clients;
 	c->mon->clients = c;
+}
+
+void
+attachaside(Client *c)
+{
+    Client *at = nexttagged(c);
+    if (!at) {
+        attach(c);
+        return;
+    }
+    c->next = at->next;
+    at->next = c;
 }
 
 void
@@ -1190,7 +1205,7 @@ manage(Window w, XWindowAttributes *wa)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
-	attach(c);
+    attachaside(c);
 	attachstack(c);
 	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
 		(unsigned char *) &(c->win), 1);
@@ -1369,6 +1384,17 @@ movestack(const Arg *arg) {
 
 		arrange(selmon);
 	}
+}
+
+Client *
+nexttagged(Client *c)
+{
+    Client *walked = c->mon->clients;
+    for(;
+            walked && (walked->isfloating || !ISVISIBLEONTAG(walked, c->tags));
+            walked = walked->next
+    );
+    return walked;
 }
 
 Client *
@@ -1600,7 +1626,7 @@ sendmon(Client *c, Monitor *m)
 	detachstack(c);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-	attach(c);
+	attachaside(c);
 	attachstack(c);
 	focus(NULL);
 	arrange(NULL);
@@ -2255,6 +2281,7 @@ updategeom(void)
 					detachstack(c);
 					c->mon = mons;
 					attach(c);
+                    attachaside(c);
 					attachstack(c);
 				}
 				if (m == selmon)
